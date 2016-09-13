d168e6e69baaa187158e47bb741430b9c69fae88
David Ungar <davidungar@us.ibm.com>
2016-09-12 17:46:59 -0700

    make apply operator internal

--- Sources/Apply.swift
+++ Sources/Apply.swift
@@ -19,6 +19,6 @@ precedencegroup LeftFunctionalApply {
 infix operator |> : LeftFunctionalApply
 
 // pipe val into monadic fn
-public func |>  <A, B> ( x: A, f: (A) throws -> B ) rethrows  -> B {
+internal func |>  <A, B> ( x: A, f: (A) throws -> B ) rethrows  -> B {
     return try f(x)
 }
