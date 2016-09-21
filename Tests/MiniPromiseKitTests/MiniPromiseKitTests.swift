e371652c6f8b6fc7a669b808db26b091f5d7b42b
David Ungar <davidungar@us.ibm.com>
2016-09-21 06:01:44 -0700

    more tests

--- Tests/MiniPromiseKitTests/MiniPromiseKitTests.swift
+++ Tests/MiniPromiseKitTests/MiniPromiseKitTests.swift
@@ -26,26 +26,164 @@ func echoPromise(message: String) -> Promise<String> {
 
 class MiniPromiseKitTests: XCTestCase {
     
-    func testBasic() {
-        
+    func testFirstlySuccess() {
         let expectation1 = expectation(description: "Get all the user feeds")
         
-        _ = firstly {
-            echoPromise(message: "Awesome")
-            }
+        _ = firstly {   echoPromise(message: "Awesome")   }
         .then { returnedMessage in
             XCTAssertEqual(returnedMessage, "Echo: Awesome")
             expectation1.fulfill()
         }
-        
+            .catch { _ in XCTFail() }
         waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    enum TestError: Error {
+        case firstlyFailure, alwaysFailure, tapFailure, thenSyncFailure, thenAsyncFailure
+    }
+    func testFirstlyFailure() {
+        let expectation1 = expectation(description: "Good")
         
+        _ = firstly { (Void) throws -> Promise<Int> in throw TestError.firstlyFailure  }
+            .then { _ -> Void in XCTFail() }
+            .catch {
+                XCTAssert( $0 as? TestError  == TestError.firstlyFailure )
+                expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testAlwaysSuccess() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly {   echoPromise(message: "Awesome")   }
+            .always {
+                expectation1.fulfill()
+            }
+            .catch { _ in XCTFail() }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testAlwaysFailure() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly { (Void) throws -> Promise<Int> in throw TestError.alwaysFailure  }
+            .always {
+                expectation1.fulfill()
+            }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testTapSuccess() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly {   echoPromise(message: "Awesome")   }
+            .tap {
+                if case let .fulfilled(msg) = $0 {
+                    XCTAssertEqual(msg, "Echo: Awesome")
+                }
+                else {
+                    XCTFail()
+                }
+                expectation1.fulfill()
+            }
+            .catch { _ in XCTFail() }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testTapFailure() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly { (Void) throws -> Promise<Int> in throw TestError.tapFailure  }
+            .tap {
+                if case let .rejected(e) = $0 {
+                    XCTAssert(e as? TestError  ==  TestError.tapFailure)
+                }
+                else {
+                    XCTFail()
+                }
+                expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testThenSyncSuccess() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        var firstThen = false
+        
+        _ = firstly {   Promise(value: "Awesome")   }
+            .then { (s: String) -> String in
+                firstThen = s == "Awesome"
+                XCTAssert(firstThen)
+                return firstThen ? "yes" : "no"
+            }
+            .then {
+                XCTAssert( firstThen &&  $0 == "yes")
+                expectation1.fulfill()
+                return
+            }
+            .catch { _ in XCTFail() }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testThenSyncFailure() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly { (Void) throws -> Promise<Int> in throw TestError.thenSyncFailure  }
+            .then { _ in XCTFail() }
+            .catch {
+                XCTAssert($0 as? TestError  ==  TestError.thenSyncFailure)
+                expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testThenAsyncSuccess() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        var firstThen = false
+        
+        _ = firstly {   Promise(value: "Awesome")   }
+            .then { (s: String) -> Promise<String> in
+                Promise<String> {
+                    fulfill, reject in
+                    DispatchQueue.global(qos: .userInitiated).async {
+                        firstThen = s == "Awesome"
+                        XCTAssert(firstThen)
+                        if firstThen { fulfill("yes") }  else { reject(TestError.thenAsyncFailure) }
+                    }
+                }
+            }
+            .then {
+                XCTAssert( firstThen &&  $0 == "yes")
+                expectation1.fulfill()
+                return
+            }
+            .catch { _ in XCTFail() }
+        waitForExpectations(timeout: 5, handler: { _ in  })
+    }
+    func testThenAsyncFailure() {
+        let expectation1 = expectation(description: "Get all the user feeds")
+        
+        _ = firstly { (Void) throws -> Promise<Int> in   throw TestError.thenAsyncFailure  }
+            .then { (Int) -> Promise<Int> in
+                Promise<Int> {
+                    fulfill, reject in
+                    XCTFail()
+                    fulfill(3)
+                }
+            }
+            .catch {
+                XCTAssert($0 as? TestError  ==  TestError.thenAsyncFailure)
+                expectation1.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: { _ in  })
     }
 
 
     static var allTests : [(String, (MiniPromiseKitTests) -> () throws -> Void)] {
         return [
-            ("testBasic", testBasic),
-        ]
+            ("testFirstlySuccess",    testFirstlySuccess),
+            ("testFirstlyFailure",    testFirstlyFailure),
+            ("testAlwaysSuccess",     testAlwaysSuccess),
+            ("testAlwaysFailure",     testAlwaysFailure),
+            ("testTapSuccess",        testTapSuccess),
+            ("testTapFailure",        testTapFailure),
+            ("testThenSyncSuccess",   testThenSyncSuccess),
+            ("testThenSyncFailure",   testThenSyncFailure),
+            ("testThenAsyncSuccess",  testThenAsyncSuccess),
+            ("testThenAsyncFailure",  testThenAsyncFailure),
+       ]
     }
 }
